a1fb485c9ed7dba2dbd096091357a0e45e7f86b3
Michael Byers <mikebyers24@gmail.com>
2024-07-30 11:31:44 -0400

    installed pandas library and added background photo to screen.

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+from turtle import Screen, Turtle
+import pandas
+
+screen = Screen()
+pic = Turtle()
+
+screen.title('US States Game')
+screen.addshape("blank_states_img.gif")
+
+turtle.shape("blank_states_img.gif")
+screen.exitonclick()
